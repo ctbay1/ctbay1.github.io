a05add52898a28f4d25f26e2002f19cc61a8456a
ctbay1 <144496681+ctbay1@users.noreply.github.com>
2023-10-04 02:59:17 +0300

    add hidden attribute change javascript

--- resources/script/script.js
+++ resources/script/script.js
@@ -1,7 +1,9 @@
 let game_start_block = document.getElementById('start_game');
+let game_grid        = document.getElementById('game_grid');
 
-function hide_start_block(event){
-    event.target.style.visibility = 'hidden';
+function hide_start_block(){
+    game_grid.hidden = false;
+    game_start_block.hidden = true;
 }
 
 game_start_block.addEventListener('click',hide_start_block);
