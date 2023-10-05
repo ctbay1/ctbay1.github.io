9b6a63b0ffe683c308cefded8e57768845611e11
ctbay1 <144496681+ctbay1@users.noreply.github.com>
2023-10-05 18:19:04 +0300

    add javascript loops for displaying blocks

--- resources/script/script.js
+++ resources/script/script.js
@@ -7,7 +7,7 @@ function hide_start_block(){
 }
 
 game_start_block.addEventListener('click',hide_start_block);
-
+/*
 const projects_block = document.getElementById('projects_title');
 const game_sec       = document.getElementById('game_sec');
 
@@ -19,4 +19,26 @@ function disp_projects_block(){
     }
 }
 
-projects_block.addEventListener('click',disp_projects_block);
+projects_block.addEventListener('click',disp_projects_block);*/
+
+let title_blocks   = document.getElementsByClassName('title');
+let section_blocks = document.getElementsByClassName('section');
+
+function disp_sec_blocks(num){
+    //console.log(num);
+    for (let i=num; i<num+1; i++){
+        //console.log(i);
+        if (section_blocks[i].hidden === true){
+            section_blocks[i].hidden = false;
+        } else{
+            section_blocks[i].hidden = true;
+        }
+    }
+}
+
+
+for (let j=0; j<title_blocks.length; j++){
+    title_blocks[j].addEventListener('click',()=>{
+        disp_sec_blocks(j);
+    });
+}
