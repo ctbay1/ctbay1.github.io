344c86f74547cb484fe1fb4fcc806dd55d626ae0
ctbay1 <144496681+ctbay1@users.noreply.github.com>
2023-10-12 17:52:49 +0300

    change game code add global vars

--- resources/script/game.js
+++ resources/script/game.js
@@ -6,7 +6,11 @@ const letter_in_but    = document.getElementById('letter_in_button');
 
 const boxes       = document.getElementsByClassName('box');
 const box_nums    = 9;
-const box_letters = document.getElementsByClassName('box_letter'); 
+const box_letters = document.getElementsByClassName('box_letter');
+
+let rand_box_list    = [];
+let control_box_list = [];
+let level            = 1;
 
 function generate_rand_num(){
     return Math.floor(Math.random()*box_nums);
@@ -20,10 +24,22 @@ function reset_color(num){
     boxes[num].style.backgroundColor = '#2F5C9E';
 }
 
+function box_clicked_color(box_int){
+    boxes[box_int].style.backgroundColor = '#84CF1B';
+}
+
+for (let i=0; i<boxes.length; i++){
+    boxes[i].addEventListener('click',()=>{
+        box_clicked_color(i);
+    })
+}
+
 function reset_all(){
     for (let i=0; i<boxes.length; i++){
         boxes[i].style.backgroundColor = '#2F5C9E'
     }
+    rand_box_list    = [];
+    control_box_list = [];
 }
 
 function create_rand_box_list(lim){
@@ -52,74 +68,62 @@ function highlight_boxes(box_num){
     },2000);
 }
 
+/*******************************/
+
+function compare_box_lists(){
+    if (rand_box_list.join('') === control_box_list.join('')){
+        window.alert(`Level ${level} clear!`);
+        level++;
+        if (level === 10){
+            window.alert(`Congratulations, you've cleared the game! You must be a smart person! Would you like to restart? Going back to the Level 1.`)
+            reset_all();
+            level = 1;
+            play_game(1);
+        }else{
+            reset_all();
+            play_game(level);
+        }
+    }
+}
+
+for (let i=0; i<boxes.length; i++){
+    boxes[i].addEventListener('click',()=>{
+
+        if (control_box_list.includes(i) === false){ 
+            control_box_list.push(i);
+            //console.log(control_box_list);
+        }
+        compare_box_lists();
+    });
+}
+
 function play_game(level){
 
-    const rand_box_list = create_rand_box_list(level - 1);
-    let control_list    = [];
+    rand_box_list = create_rand_box_list(level - 1);
+    //console.log(rand_box_list);
     for (let i=0; i<rand_box_list.length; i++){
         setTimeout(()=>{
             highlight_boxes(rand_box_list[i]);
         },1000*i);
-        let num = rand_box_list[i];
-        boxes[num].addEventListener('click',()=>{
-            /*console.log(control_list);
-            console.log(rand_box_list);*/
-            if (control_list.includes(num) === false){ //wrote this because couldn't add removeEventListener,
-                control_list.push(num); //it's here to make sure you don't add twice to the list when accidentally double clicked.
-            }
-            /*console.log(control_list);
-            console.log(rand_box_list);*/
-            /*if ((control_list.length === rand_box_list.length) && (rand_box_list.join('') !== control_list.join(''))){
-                window.alert('Wrong order, try again!');
-                control_list = ['x','y','z'];
-                reset_all();
-                play_game(level);
-            }*/
-            /*console.log(control_list);
-            console.log(rand_box_list);*/
-            if (rand_box_list.join('') === control_list.join('')){
-                window.alert(`Level ${level} clear!`);
-                control_list = ['a','b','c'];
-                level++;
-                if (level === 10){
-                    window.alert(`Congratulations, you've cleared the game! You must be a smart person! Would you like to restart? Going back to the Level 1.`)
-                    reset_all();
-                    play_game(1);
-                }else{
-                    reset_all();
-                    play_game(level);
-                }
-            }
-            /*console.log(control_list);
-            console.log(rand_box_list);*/
-        });
     }
 }
+
+/*******************************/
 
 function hide_start_block(){
     if (game_grid.hidden === true){
         game_grid.hidden = false;
         game_start_block.hidden = true;
 
-        const level = 1;
         play_game(level);
     }
 }
 
 game_start_block.addEventListener('click',hide_start_block);
 
-function box_clicked_color(box_int){
-    boxes[box_int].style.backgroundColor = '#84CF1B';
-}
-
-for (let i=0; i<boxes.length; i++){
-    boxes[i].addEventListener('click',()=>{
-        box_clicked_color(i);
-    })
-}
-
 function restart_game(){
     reset_all();
+    level = 1;
     play_game(1);
 }
 
